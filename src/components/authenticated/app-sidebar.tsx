418fa39b92ecccce97e76e96aa631bdfd4e7b24a
Y-Mitsuishi0601 <yuk20040601@gmail.com>
2025-08-04 18:33:57 +0900

    add Workspace dropdown on Sidebar

--- src/components/authenticated/app-sidebar.tsx
+++ src/components/authenticated/app-sidebar.tsx
@@ -1,20 +1,94 @@
+import { Folder, Home, User, ChartBarDecreasing, Settings, ChevronDown } from "lucide-react"
+
 import {
   Sidebar,
-  SidebarContent,
-  SidebarFooter,
-  SidebarGroup,
   SidebarHeader,
+  SidebarContent,
+  SidebarGroup,
+  SidebarGroupContent,
+  SidebarGroupLabel,
+  SidebarMenu,
+  SidebarMenuButton,
+  SidebarMenuItem,
 } from "@/components/ui/sidebar"
+
+import {
+  DropdownMenu,
+  DropdownMenuContent,
+  DropdownMenuItem,
+  DropdownMenuTrigger,
+} from "@/components/ui/dropdown-menu"
+
+
+
+// Menu items.
+const items = [
+  {
+    title: "Projects",
+    url: "/projects",
+    icon: Folder,
+  },
+  {
+    title: "Dashboard",
+    url: "/dashboard",
+    icon: ChartBarDecreasing,
+  },
+  {
+    title: "Member",
+    url: "/members",
+    icon: User,
+  },
+  {
+    title: "Settings",
+    url: "settings",
+    icon: Settings,
+  },
+]
 
 export function AppSidebar() {
   return (
     <Sidebar>
-      <SidebarHeader />
-      <SidebarContent>
-        <SidebarGroup />
-        <SidebarGroup />
-      </SidebarContent>
-      <SidebarFooter />
+      <SidebarHeader>
+        <SidebarMenu>
+          <SidebarMenuItem>
+            <DropdownMenu>
+              <DropdownMenuTrigger asChild>
+                <SidebarMenuButton>
+                  Acme Inc
+                  <ChevronDown className="ml-auto" />
+                </SidebarMenuButton>
+              </DropdownMenuTrigger>
+              <DropdownMenuContent className="w-[--radix-popper-anchor-width]">
+                <DropdownMenuItem>
+                  <span>Acme Inc</span>
+                </DropdownMenuItem>
+                <DropdownMenuItem>
+                  <span>Acme Corp.</span>
+                </DropdownMenuItem>
+              </DropdownMenuContent>
+            </DropdownMenu>
+          </SidebarMenuItem>
+        </SidebarMenu>
+        <SidebarContent>
+          <SidebarGroup>
+            <SidebarGroupLabel>Menu</SidebarGroupLabel>
+            <SidebarGroupContent>
+              <SidebarMenu>
+                {items.map((item) => (
+                  <SidebarMenuItem key={item.title}>
+                    <SidebarMenuButton asChild>
+                      <a href={item.url}>
+                        <item.icon />
+                        <span>{item.title}</span>
+                      </a>
+                    </SidebarMenuButton>
+                  </SidebarMenuItem>
+                ))}
+              </SidebarMenu>
+            </SidebarGroupContent>
+          </SidebarGroup>
+        </SidebarContent>
+      </SidebarHeader>
     </Sidebar>
   )
 }
