926c7e3e7d236087a18bf363b5f23a47dd34d9f3
Y-Mitsuishi0601 <yuk20040601@gmail.com>
2025-08-08 16:43:54 +0900

    import useQuery

--- src/app/(authenticated)/layout.tsx
+++ src/app/(authenticated)/layout.tsx
@@ -1,6 +1,12 @@
 import type { Metadata } from "next";
 import { SidebarProvider, SidebarTrigger } from "@/components/ui/sidebar"
 import { AppSidebar } from "@/app/(authenticated)/_components/app-sidebar";
+import {
+  QueryClient,
+  QueryClientProvider,
+} from '@tanstack/react-query';
+import { ReactQueryDevtools } from '@tanstack/react-query-devtools';
+
 
 
 export const metadata: Metadata = {
@@ -11,11 +17,14 @@ export const metadata: Metadata = {
 export default function AuthenticatedLayout({ children }: { children: React.ReactNode }) {
   return (
     <>
-      <SidebarProvider>
-        <AppSidebar />
-        <SidebarTrigger />
-        {children}
-      </SidebarProvider>
+      <QueryClientProvider client={new QueryClient()}>
+        <SidebarProvider>
+          <AppSidebar />
+          <SidebarTrigger />
+          {children}
+        </SidebarProvider>
+        <ReactQueryDevtools initialIsOpen={false} />
+      </QueryClientProvider>
     </>
   );
 }
